7acedb2692051483d2660d55f71eb6cfa8a9073c
Dimitri Kurashvili <dimakura@gmail.com>
2014-07-01 14:32:07 +0400

    display main task information

--- src/gse/pathfinder/TaskActivity.java
+++ src/gse/pathfinder/TaskActivity.java
@@ -1,15 +1,45 @@
 package gse.pathfinder;
 
+import gse.pathfinder.models.Task;
+import gse.pathfinder.ui.BaseActivity;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.app.Activity;
+import android.text.SpannableString;
+import android.text.style.StyleSpan;
 import android.view.Menu;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class TaskActivity extends Activity {
+public class TaskActivity extends BaseActivity {
+	private ImageView	imgStatus;
+	private TextView	txtNumber;
+	private TextView	txtDate;
+	private TextView	txtNote;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_task);
+		imgStatus = (ImageView) findViewById(R.id.status_image_task_activity);
+		txtNumber = (TextView) findViewById(R.id.number_task_activity);
+		txtDate = (TextView) findViewById(R.id.date_task_activity);
+		txtNote = (TextView) findViewById(R.id.note_task_activity);
+		txtNote.setTextColor(Color.GRAY);
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		Task task = (Task) getIntent().getExtras().get("task");
+
+		String numberText = " #" + task.getNumber();
+		SpannableString spanString = new SpannableString(numberText);
+		spanString.setSpan(new StyleSpan(Typeface.BOLD), 0, spanString.length(), 0);
+		txtNumber.setText(spanString);
+		imgStatus.setImageResource(task.getStatusImage());
+		txtDate.setText(TasksActivity.DATE_FORMAT.format(task.getCreatedAt()));
+		txtNote.setText(task.getNote());
 	}
 
 	@Override
