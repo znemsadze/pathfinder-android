85d3d991e01bd2b2e9b6c67b5a6371629faae9f4
Dimitri Kurashvili <dimakura@gmail.com>
2014-07-15 10:41:10 +0400

    save username/password

--- src/gse/pathfinder/LoginActivity.java
+++ src/gse/pathfinder/LoginActivity.java
@@ -6,17 +6,22 @@ import gse.pathfinder.models.User;
 import gse.pathfinder.ui.BaseActivity;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.TextView;
 
 public class LoginActivity extends BaseActivity {
-	private TextView	     txtUsername;
-	private TextView	     txtPassword;
-	private TextView	     txtServer;
-	private ProgressDialog	waitDialog;
+	private TextView txtUsername;
+	private TextView txtPassword;
+	private TextView txtServer;
+	private ProgressDialog waitDialog;
+	private CheckBox saveLogin;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +30,46 @@ public class LoginActivity extends BaseActivity {
 		txtUsername = (TextView) findViewById(R.id.username_login);
 		txtPassword = (TextView) findViewById(R.id.password_login);
 		txtServer = (TextView) findViewById(R.id.server_login);
+		saveLogin = (CheckBox) findViewById(R.id.savelogin_login);
+		saveLogin.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				if (!isChecked) {
+					clearUsernameAndPassword();
+				}
+			}
+		});
 	}
 
 	@Override
 	protected void onStart() {
 		super.onStart();
 		txtServer.setText(NetworkUtils.getDefaultHost(this));
+		SharedPreferences prefs = Preferences.getPreference(this);
+		boolean savelogin = prefs.getBoolean("savelogin", true);
+		saveLogin.setChecked(savelogin);
+		if (savelogin) {
+			txtUsername.setText(Crypto.decrypt(prefs.getString("username", null)));
+			txtPassword.setText(Crypto.decrypt(prefs.getString("password", null)));
+		}
+	}
+
+	private void saveUsernameAndPassword(String username, String password) {
+		SharedPreferences pref = Preferences.getPreference(this);
+		SharedPreferences.Editor editor = pref.edit();
+		editor.putString("username", Crypto.encrypt(username));
+		editor.putString("password", Crypto.encrypt(password));
+		editor.putBoolean("savelogin", true);
+		editor.commit();
+	}
+
+	private void clearUsernameAndPassword() {
+		SharedPreferences pref = Preferences.getPreference(this);
+		SharedPreferences.Editor editor = pref.edit();
+		editor.remove("username");
+		editor.remove("password");
+		editor.putBoolean("savelogin", false);
+		editor.commit();
 	}
 
 	@Override
@@ -61,6 +100,7 @@ public class LoginActivity extends BaseActivity {
 		}
 		waitDialog = ProgressDialog.show(this, "გთხოვთ დაელოდეთ", "სერვერთან დაკავშირება...");
 		new LoginTask().execute(username, password, host);
+		if (saveLogin.isChecked()) saveUsernameAndPassword(username, password);
 	}
 
 	void userLoggedIn(User user) {
@@ -70,7 +110,7 @@ public class LoginActivity extends BaseActivity {
 	}
 
 	private class LoginTask extends AsyncTask<String, Void, User> {
-		private Exception	exception;
+		private Exception exception;
 
 		@Override
 		protected User doInBackground(String... params) {
