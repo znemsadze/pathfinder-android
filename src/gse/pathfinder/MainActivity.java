7fd78c710c9ae3f688d1fb5be73834f5585619d4
Dimitri Kurashvili <dimakura@gmail.com>
2014-07-15 15:55:47 +0400

    removing options menu in main activity

--- src/gse/pathfinder/MainActivity.java
+++ src/gse/pathfinder/MainActivity.java
@@ -30,13 +30,6 @@ public class MainActivity extends BaseActivity {
 		}
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
 	public void onLogout(View view) {
 		ApplicationController.logout(this);
 		TrackingActivity.stopTracking(this);
