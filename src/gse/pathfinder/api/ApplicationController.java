c169dc6934056c6b71c602986b965908cc2979d2
Dimitri Kurashvili <dimakura@gmail.com>
2014-10-05 20:35:37 +0400

    v13

--- src/gse/pathfinder/api/ApplicationController.java
+++ src/gse/pathfinder/api/ApplicationController.java
@@ -21,7 +21,7 @@ import android.content.Context;
 import android.net.Uri;
 
 public class ApplicationController {
-	public static final String VERSION = "012";
+	public static final String VERSION = "013";
 
 	// -- user
 
